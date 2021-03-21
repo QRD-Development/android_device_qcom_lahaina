be8ad629ccb9923f36d800d40af4078f72785623
ZVNexus <akoskovich@pm.me>
2021-03-20 19:23:42 -0700

    lahaina: Declare support for device ID attestation.
    Change-Id: I0365d6726d687af290b1ba439fd558c4445f5d7f

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 TARGET_BOARD_PLATFORM := lahaina
 TARGET_BOOTLOADER_BOARD_NAME := lahaina
 
+# Attestation
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
 # Authsecret
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
