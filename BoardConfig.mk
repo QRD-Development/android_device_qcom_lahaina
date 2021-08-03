4b8656d023bbfff8b7d5cf43da6f270cd4a39d6d
Roopesh Nataraja <roopeshr@codeaurora.org>
2021-08-03 16:58:43 -0700

    lahaina: Use sha256 to build hashtree for dm-verity partitions
    sha256 provides more robustness against malicious attacks.
    Hence, use it to build hashtree for dm-verity partitions.
    
    Change-Id: I84bf848e7dc2cf3f9d54c6ed76d0f03e5ab93c6b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -260,8 +260,11 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
-# Use sha256 hash algorithm for odm vendor_dlkm vendor partition
+# Use sha256 for dm-verity partitions
 BOARD_AVB_ODM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
+BOARD_AVB_PRODUCT_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
+BOARD_AVB_SYSTEM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
+BOARD_AVB_SYSTEM_EXT_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 BOARD_AVB_VENDOR_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 BOARD_AVB_VENDOR_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 
