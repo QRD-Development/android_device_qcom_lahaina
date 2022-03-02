8af62aab032ec042ef7e0913543697285b4798e4
Alone0316 <rutujamandlik06@gmail.com>
2022-03-02 06:56:08 +0000

    lahaina: make vendor variant of libstagefrighthw
    E vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -394,6 +394,10 @@ PRODUCT_PACKAGES += \
     NfcNci \
     Tag
 
+# Omx
+PRODUCT_PACKAGES += \
+    libstagefrighthw.vendor
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
