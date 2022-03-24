dd773eef06a943da8b3e7f1eeb395c9af1395bc0
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    lahaina: Inherit launch_with_vendor_ramdisk for vAB
    - sm8350 devices have a vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- lahaina.mk
+++ lahaina.mk
@@ -7,7 +7,7 @@
 DEVICE_PATH := device/qcom/lahaina
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti \
