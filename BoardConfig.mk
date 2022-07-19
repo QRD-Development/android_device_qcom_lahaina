47ea978572572812df1b2e2aa185440a9a26b188
Jake Weinstein <jake@aospa.co>
2022-07-19 14:52:50 -0500

    lahaina: Remove console and video from cmdline
    These are unnecessary for production builds and are a waste
    of memory for our purposes.
    
    Change-Id: I9a52f989804ed9fd2d2e4ed53061281a408bd2fe

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,12 +60,9 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_lahaina
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
-    console=ttyMSM0,115200n8 \
     androidboot.hardware=qcom \
-    androidboot.console=ttyMSM0 \
     androidboot.memcg=1 \
     lpm_levels.sleep_disabled=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     androidboot.usbcontroller=a600000.dwc3 \
