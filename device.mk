76d00c47a7461a6a0b04334e1e553f146eebfe08
Jake Weinstein <jake@aospa.co>
2022-07-23 06:25:18 +0900

    lahaina: Reorganize device makefile
    Change-Id: Iec95cd6bceed0111984004a18caf9f2ed0c2438a

---
+++ device.mk
@@ -0,0 +1,137 @@
+# A/B
+ENABLE_AB ?= true
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_client \
+    update_verifier \
+    android.hardware.boot@1.1-impl-qti \
+    android.hardware.boot@1.1-impl-qti.recovery \
+    android.hardware.boot@1.1-service
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+# API
+BOARD_SHIPPING_API_LEVEL := 30
+BOARD_API_LEVEL := 30
+SHIPPING_API_LEVEL := 30
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# Architecture
+TARGET_BOARD_PLATFORM := lahaina
+TARGET_BOOTLOADER_BOARD_NAME := lahaina
+
+# Audio
+USE_LIB_PROCESS_GROUP := true
+
+# AVB
+BOARD_AVB_ENABLE := true
+
+# Build
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.device@3.2-impl \
+    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
+# Emulated Storage
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Filesystem
+PRODUCT_PACKAGES += \
+    fs_config_files
+
+# Fingerprint
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+# FM
+BOARD_HAVE_QCOM_FM := false
+
+# Generic Kernel Headers
+TARGET_HAS_GENERIC_KERNEL_HEADERS := true
+
+# Init
+PRODUCT_PACKAGES += \
+    fstab.default \
+    init.qti.ufs.rc \
+    init.target.rc \
+    ueventd.lahaina.rc
+
+# Kernel Modules
+KERNEL_MODULES_INSTALL := dlkm
+KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
+# Key Layout
+PRODUCT_PACKAGES += \
+    gpio-keys.kl
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.qti \
+    lights.qcom
+
+# Manifests
+DEVICE_MANIFEST_FILE := device/qcom/lahaina/manifest_lahaina.xml
+DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
+
+# Partitions - Dynamic
+PRODUCT_BUILD_ODM_IMAGE := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
+
+$(call inherit-product, build/make/target/product/gsi_keys.mk)
+
+# Partitions - FRP
+BOARD_FRP_PARTITION_NAME := frp
+
+# Partitions - Vendor
+ENABLE_VENDOR_IMAGE := true
+
+# QRTR
+PRODUCT_PACKAGES += \
+    qrtr-ns \
+    qrtr-lookup \
+    libqrtr
+
+# QSSI
+TARGET_USES_QSSI := true
+
+# QTI Components
+TARGET_COMMON_QTI_COMPONENTS := all
+
+# RRO
+TARGET_USES_RRO := true
+
+# Storage
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Treble
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_VENDOR_MOVE_ENABLED := true
+PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
+TARGET_MOUNT_POINTS_SYMLINKS := false
