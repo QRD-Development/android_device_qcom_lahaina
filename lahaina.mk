d26db449546767f0ce6c6758d397ddd5eaa3ab5c
Vaisakh Murali <mvaisakh@statixos.com>
2022-08-22 03:12:47 -0400

    lahaina: Switch to AIDL clearkey service
    Change-Id: I39609424434d87b92e2215680c6f62f83ae9f4d3

--- lahaina.mk
+++ lahaina.mk
@@ -89,7 +89,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # Fastbootd
 PRODUCT_PACKAGES += \
