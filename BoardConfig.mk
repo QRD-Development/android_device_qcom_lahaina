c734e6e30f269abd823eafe7d457292eb89ca352
Cyber Knight <cyberknight755@gmail.com>
2022-10-21 23:56:34 +0800

    lahaina: Drop androidboot.memcg=1 kernel CMDLINE
    - We are not on a low-end device to use memcg hence let's drop it.
    
     * [EndCredits]: We no longer need this since we've enabled mulit-gen
       lru framework to manage memory
    
    Change-Id: Ib87bc8d2970f1d978c5027f10e48db5d27b96945

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,6 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
-    androidboot.memcg=1 \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
