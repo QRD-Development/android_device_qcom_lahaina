30375d1e475fcae2f475195752f0b4b96749e210
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-24 23:30:29 +0100

    lahaina: Drop SDM LiveDisplay service
    * We switched to AOSP colors so messing with color calibration will
      confuse QDCM
    
    Change-Id: Ia22c4c06e922d1b7f9c60b257299634333cc3fe4

--- device.mk
+++ device.mk
@@ -269,10 +269,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.lights.service
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
