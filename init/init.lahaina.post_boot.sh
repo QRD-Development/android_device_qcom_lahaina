38a8226ca3e2b785c1d4d2cf5d4c9b2b661f5a24
Wei Wang <wvw@google.com>
2023-03-05 00:50:04 +0600

    lahaina: init: Boot time tuning
    Test: build and boottime test
    Change-Id: Ib8f31d39501b4747f0c268d2dd87e814bcc7843b
    Signed-off-by: Wei Wang <wvw@google.com>

--- init/init.lahaina.post_boot.sh
+++ init/init.lahaina.post_boot.sh
@@ -4,3 +4,17 @@
 #
 
 # Custom tuning for Lahaina SoC
+
+# Setup runtime schedTune
+echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
+echo "schedutil" > /sys/devices/system/cpu/cpu7/cpufreq/scaling_governor
+echo 1 > /dev/stune/foreground/schedtune.prefer_idle
+echo 0 > /dev/stune/foreground/schedtune.prefer_high_cap
+echo 0 > /dev/stune/foreground/schedtune.boost
+echo 0 > /dev/stune/schedtune.prefer_idle
+echo 0 > /dev/stune/schedtune.prefer_high_cap
+echo 0 > /dev/stune/schedtune.boost
+echo 1 > /dev/stune/top-app/schedtune.prefer_idle
+echo 0 > /dev/stune/top-app/schedtune.prefer_high_cap
+echo 10 > /dev/stune/top-app/schedtune.boost 10
