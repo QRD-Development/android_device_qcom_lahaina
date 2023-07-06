f4252daba7a1241a7ed070fdfa2797cb366aed18
Kunmun <kunmun@aospa.co>
2023-07-06 09:02:03 +0530

    lahaina: Makefile: Copy graphics common ndk_platform libs from VNDK
    Change-Id: Idf0c6c2c6839b2ab81dabab878e9f8dbc04525d9

--- device.mk
+++ device.mk
@@ -198,7 +198,9 @@ NEED_AIDL_NDK_PLATFORM_BACKEND := true
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib/libui-v30.so
+    prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib/libui-v30.so \
+    prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-sp/android.hardware.common-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.common-V1-ndk_platform.so \
+    prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-sp/android.hardware.graphics.common-V1-ndk_platform.so:$(TARGET_COPY_OUT_VENDOR)/lib/android.hardware.graphics.common-V1-ndk_platform.so
 
 # Vibrator
 PRODUCT_PACKAGES += \
