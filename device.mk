bc4f80edfebf379fa694257c998acad9c917d308
Bruno Martins <bgcngm@gmail.com>
2023-07-25 22:28:38 +0100

    lahaina: Add device ID attestation support
    Change-Id: I30c3ffb6cdc816c69d9f0ecc52e87c7cd8f6fa8e

--- device.mk
+++ device.mk
@@ -256,6 +256,9 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml
 
 # Keymaster
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
+
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
