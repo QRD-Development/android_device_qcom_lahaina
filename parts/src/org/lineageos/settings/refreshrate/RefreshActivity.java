085246896d5a46b68f4d8a88bca1fe10d4e2fef2
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    lahaina: parts: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
+++ parts/src/org/lineageos/settings/refreshrate/RefreshActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.refreshrate;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class RefreshActivity extends CollapsingToolbarBaseActivity {
     private static final String TAG_REFRESH = "refresh";
@@ -28,7 +27,7 @@ public class RefreshActivity extends CollapsingToolbarBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+        getFragmentManager().beginTransaction().replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
                 new RefreshSettingsFragment(), TAG_REFRESH).commit();
     }
 }
