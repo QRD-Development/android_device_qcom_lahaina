79f600331e56149ba5940298725666ddd611d6d9
ReallySnow <reallysnow233@gmail.com>
2023-11-13 15:57:22 +0800

    lahaina: Build camera HIDL & copy camera permission
    Change-Id: Ib8ac027341140550f4c839246cd85757659a19d5

--- lahaina.mk
+++ lahaina.mk
@@ -44,6 +44,19 @@ TARGET_BOOTLOADER_BOARD_NAME := lahaina
 # AVB
 BOARD_AVB_ENABLE := true
 
+# Camera
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libcamera2ndk_vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
