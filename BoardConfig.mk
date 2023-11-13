a9dfc84df69de690149c4192d3c25af6a8511e6a
ReallySnow <reallysnow233@gmail.com>
2023-11-13 22:24:35 +0800

    lahaina: Add device framework matrix
    Change-Id: I67e85b5a8509a2d2e2256cab3778f47e89a9366f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,6 +140,9 @@ SOONG_CONFIG_ufsbsg += ufsframework
 SOONG_CONFIG_ufsbsg_ufsframework := bsg
 
 # VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml
+
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest_ab.xml \
     $(DEVICE_PATH)/configs/vintf/manifest_lahaina.xml 
