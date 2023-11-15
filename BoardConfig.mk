bd25c8d54b2dfdafeb6d6283039b943edd0de631
ReallySnow <reallysnow233@gmail.com>
2023-11-15 21:00:18 +0800

    lahaina: Initial properties
    Change-Id: If9dfa87e7ff1fa5480fc24700f5aba91e94b6012

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,19 @@ BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 6438256640
 BOARD_SUPER_PARTITION_SIZE := 6442450944
 BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 0x06000000
 
+# Properties
+TARGET_ODM_PROP += \
+    $(DEVICE_PATH)/configs/properties/odm.prop
+
+TARGET_PRODUCT_PROP += \
+    $(DEVICE_PATH)/configs/properties/product.prop
+
+TARGET_SYSTEM_PROP += \
+    $(DEVICE_PATH)/configs/properties/system.prop
+
+TARGET_VENDOR_PROP += \
+    $(DEVICE_PATH)/configs/properties/vendor.prop
+
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_USES_RECOVERY_AS_BOOT := true
