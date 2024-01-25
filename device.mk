de27338895e9a9fe959b885024505ee64fe35cd6
Luofan Chen <clfbbn@gmail.com>
2024-01-25 08:00:01 +0800

    lahaina: Build usb init scripts
    Change-Id: Ib043e2aedbcdd57005aff8efc737064ddaf1d5e6

--- device.mk
+++ device.mk
@@ -428,7 +428,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service-qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
