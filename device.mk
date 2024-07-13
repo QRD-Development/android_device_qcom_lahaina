1ef2f40cbabfc90c125f28eca5081c52ab7eec34
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    lahaina: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- device.mk
+++ device.mk
@@ -222,8 +222,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V1-ndk_platform.vendor \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libmemutils \
@@ -244,7 +242,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.display.mapperextensions@1.1.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # DRM
 PRODUCT_PACKAGES += \
