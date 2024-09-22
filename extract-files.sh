858567da38b451c24154835cf5d877f61b07fdcb
Aaron Kling <webgeek1234@gmail.com>
2024-09-23 02:09:02 +0900

    lahaina: Remove software omx codec references
    These are no longer available as of Android 14 QPR3. The C2 codecs
    available by default in the swcodec apex will be used instead.
    
    Change-Id: I8cdf3e700823779a7382c66c3df8410d41209b7c
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- extract-files.sh
+++ extract-files.sh
@@ -59,6 +59,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libcrypto-v33.so" "${2}" || "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "$2"
             ;;
+        vendor/etc/media_codecs.xml|vendor/etc/media_codecs_lahaina.xml|vendor/etc/media_codecs_lahaina_vendor.xml|vendor/etc/media_codecs_performance.xml|vendor/etc/media_codecs_performance_lahaina.xml|vendor/etc/media_codecs_performance_lahaina_vendor.xml|vendor/etc/media_codecs_performance_shima_v1.xml|vendor/etc/media_codecs_performance_shima_v2.xml|vendor/etc/media_codecs_performance_shima_v3.xml|vendor/etc/media_codecs_performance_yupik_iot.xml|vendor/etc/media_codecs_performance_yupik_v0.xml|vendor/etc/media_codecs_performance_yupik_v1.xml)
+            sed -Ei "/media_codecs_(google_audio|google_telephony|vendor_audio)/d" "${2}"
+            ;;
      esac
 }
 
