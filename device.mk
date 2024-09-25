c64da9d8860530f138392f2f21b34e837d0d4fb9
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    lahaina: Switch to AIDL thermal HAL
    Patch Fix for : https://github.com/QRD-Development/android_device_qcom_lahaina/commit/5aece79b6ff5f5a716fae3d55520359220fdba8d
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- device.mk
+++ device.mk
@@ -536,6 +536,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.lahaina
