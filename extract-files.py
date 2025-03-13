944daa6f7d140004962b602434f8883df686220d
Mikooo <mikooomichael@gmail.com>
2025-03-13 14:22:06 -0400

    lahaina: Switch to py extract utils
    Change-Id: I108033ab1960297b1708555c68ebf7d15772f4e8
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
+    ('vendor/etc/libnfc-mtp-SN100.conf', 'vendor/etc/libnfc-nxp.conf', 'vendor/etc/libnfc-qrd-SN100.conf'): blob_fixup()
+        .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/etc/libnfc-nci.conf': blob_fixup()
+        .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
+    'vendor/etc/msm_irqbalance.conf': blob_fixup()
+        .regex_replace('IGNORED_IRQ=27,23,38$', 'IGNORED_IRQ=27,23,38,115,332'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'lahaina',
+    'qcom',
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
