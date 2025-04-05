401f5bb00a8a6b44574c973b407c247a1935bdee
LuK1337 <priv.luk@gmail.com>
2025-04-05 22:18:31 +0200

    lahaina: libwfdservice_shim_v1 -> libwfdservice_shim
    Change-Id: Idf65dd61a2de80dfcbf72a8b75b6ca59234df573

--- extract-files.py
+++ extract-files.py
@@ -16,7 +16,7 @@ from extract_utils.main import (
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/bin/wfdservice': blob_fixup()
-        .add_needed('libwfdservice_shim_v1.so'),
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so'),
     ('vendor/etc/libnfc-mtp-SN100.conf', 'vendor/etc/libnfc-nxp.conf', 'vendor/etc/libnfc-qrd-SN100.conf'): blob_fixup()
