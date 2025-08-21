5aece79b6ff5f5a716fae3d55520359220fdba8d
npjohnson <johnsonnolen@gmail.com>
2025-08-21 20:35:53 +0800

    lahaina: Cleanup dead targets
    Offending entries:
    android.hardware.thermal@2.0-service.qti
    android.hardware.usb@1.3-service-qti
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -519,10 +519,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
-
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.lahaina
@@ -542,7 +538,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti \
     init.qcom.usb.rc \
     init.qcom.usb.sh
 
