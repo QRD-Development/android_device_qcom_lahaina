fee411e87a4047177a379db2f8072d00e6986f7d
uwu-gl <moegluwu0@gmail.com>
2026-02-25 07:26:16 +0800

    lahaina: Kang qspm seccomp policy from XQ-BC72_Customized HK_61.2.A.0.472A
    Change-Id: I036eafc32b547581d331f9212c2806c1842fd5e6

--- device.mk
+++ device.mk
@@ -567,6 +567,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     vndservicemanager
 
+# QSPM seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/seccomp_policy/qspm.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/qspm.policy
+
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
