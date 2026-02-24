d1403f3e3d45933db0035cc21cf624842e3f8d7c
uwu-gl <moegluwu0@gmail.com>
2026-02-25 07:26:48 +0800

    lahaina: Clean up qccsyshal blobs
    Change-Id: Ib44931fcb6507734aa9e0efdeb63fd06b03c31ef
    Signed-off-by: uwu-gl <moegluwu0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,6 @@ LOC_HIDL_VERSION := 4.3
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/framework_manifest.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
